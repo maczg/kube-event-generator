730db616b4740aea6a38d42756a599280473e077
maczg <massi.gollo@gmail.com>
2025-03-07 00:46:59 +0100

    add utils k8s clientset factory

--- pkg/utils/kube_manager.go
+++ pkg/utils/kube_manager.go
@@ -1,6 +1,13 @@
 package utils
 
-import "k8s.io/client-go/kubernetes"
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"os"
+)
 
 type KubernetesManager struct {
 	clientset *kubernetes.Clientset
@@ -16,4 +23,21 @@ func NewKubernetesManager(clientset *kubernetes.Clientset) *KubernetesManager {
 
 func (m *KubernetesManager) Clientset() *kubernetes.Clientset {
 	return m.clientset
+}
+
+func MakeClientSet() (*kubernetes.Clientset, error) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		logrus.Warnf("%v", err)
+		kubeconfig := fmt.Sprintf("%s/.kube/config", os.Getenv("HOME"))
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get kubeconfig: %v", err)
+		}
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create clientset: %v", err)
+	}
+	return clientset, nil
 }
